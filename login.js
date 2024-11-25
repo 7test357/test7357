c53c2b10925524516b4c213c5db7c13d8e7df559
7test357 <7test357@gmail.com>
2024-11-25 20:10:53 +0900

    Update login.js

--- login.js
+++ login.js
@@ -7,13 +7,16 @@ window.onload = function() {
         let inputPassword = document.getElementById('password').value;
 
         if (inputUsername === username && inputPassword === password) {
+            // 로그인 성공 시
             document.getElementById('mainContent').style.display = 'block';
             document.getElementById('loginForm').style.display = 'none';
         } else {
+            // 로그인 실패 시
             alert('로그인 실패: 아이디 또는 비밀번호가 잘못되었습니다.');
         }
     });
 
+    // 각 버튼 클릭 시 페이지 이동
     document.getElementById('fbButton').addEventListener('click', function() {
         window.location.href = '/fb/index.html';
     });
